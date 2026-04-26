260f2bf54ca97501ac6fda07ad26fa359fb7dca1
Hannah <hannahajibola094@gmail.com>
2026-04-26 19:16:21 +0100

    nawa o

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css"
 const App =()=> {
   return(
-    <p>
-    <button>
-      Click Me
-    </button>
-    </p>
+    <div className="App">
+      <header className="header">
+        <nav className="navbar">
+          <ul className="nav-list">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   )
 }
 
