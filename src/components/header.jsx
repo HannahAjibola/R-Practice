fae44879cc4199b2bdf6c9c71bd81c0f810eed87
Tbelzbby <belloafeez28@gmail.com>
2026-04-26 19:43:36 +0100

    refactor: import Header in App and switch to semantic CSS selectors
    - Fix Header component: rename to PascalCase and correct default export
    - Import and use Header component in App.jsx instead of duplicating markup
    - Remove all classNames in favor of semantic HTML tags
    - Update header.css to target semantic selectors (header, nav, ul, a)
    - Clear App.css to avoid duplicate styles with header.css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,10 @@
 import "./header.css"
-const header =()=> {
+const Header =()=> {
   return(
-    <div className="App">
-      <header className="header">
-        <nav className="navbar">
-          <ul className="nav-list">
+    <div>
+      <header>
+        <nav>
+          <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
@@ -19,4 +19,4 @@ const header =()=> {
 }
 
 
-export default App;
+export default Header;
