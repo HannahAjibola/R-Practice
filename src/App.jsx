4edefee8ab986bd71ea3a5a699e6d2b12b212e34
Hannah <hannahajibola094@gmail.com>
2026-04-26 19:27:00 +0100

    Wsss

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ const App =()=> {
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
+            <li><a href="#stack">Stack</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
           </ul>
         </nav>
       </header>
