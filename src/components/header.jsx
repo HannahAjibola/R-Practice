0f30bc75fc15f6b06045a8d3ef9ab325b3f82f70
Hannah <hannahajibola094@gmail.com>
2026-04-26 19:34:26 +0100

    Changed func name $ css impt Namw

---
+++ src/components/header.jsx
@@ -0,0 +1,22 @@
+import "./header.css"
+const header =()=> {
+  return(
+    <div className="App">
+      <header className="header">
+        <nav className="navbar">
+          <ul className="nav-list">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#stack">Stack</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+
+export default App;
